2e813555341ee994d143d0b8b2091cc9d1f7ef37
Joeri Sebrechts <joeri@sebrechts.net>
2025-05-29 14:03:49 +0200

    fade between pages

--- public/src/App.js
+++ public/src/App.js
@@ -1,15 +1,18 @@
+import '../lib/view-transition.js';
 import './Home.js';
 import './Details.js';
 
 customElements.define('demo-app', class extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <view-route path="/(?:index.html)?" exact>
-            <demo-home></demo-home>
-        </view-route>
-        <view-route path="/video">
-            <demo-details></demo-details>
-        </view-route>
+        <view-transition>
+            <view-route path="/(?:index.html)?" exact>
+                <demo-home></demo-home>
+            </view-route>
+            <view-route path="/video">
+                <demo-details></demo-details>
+            </view-route>
+        </view-transition>
         `;
     }
 });
